912cfcd6c2711266575e298453fd705e54975662
shishirranjan08 <shishirranjan08@gmail.com>
2017-10-12 15:36:21 +0530

    pom file added

---
+++ camel-spring/src/test/java/com/citibank/CamelSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.citibank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
